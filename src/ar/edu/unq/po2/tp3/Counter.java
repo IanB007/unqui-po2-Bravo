d5ba7a7bda85ef638777124b18192c38975841ad
IanB007 <Ian_b@192.168.0.70>
2024-03-27 04:27:41 -0300

    ContarImpares

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,52 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+
+public class Counter {
+
+	private ArrayList<Integer> numeros;
+	
+	//constructor
+	public Counter() {
+        numeros = new ArrayList<Integer>(); //inicializo la lista numeros.
+    }
+	
+	//mensajes
+	//Agrega un numero a la lista de numeros enteros creada.
+	public void agregarNumero(int numero) {
+		numeros.add(numero);
+	}
+	
+	//Cuenta la cantidad de numeros pares de la lista de enteros creada.
+	public int contarNumerosPares() {
+		int cantNumerosPares = 0;
+		for (int numero:numeros){
+			if((numero % 2) == 0) {
+				cantNumerosPares++;
+			}
+		}
+		return cantNumerosPares;
+	}
+	
+	//Cuenta la cantidad de numeros impares de la lista de enteros creada.
+	public int contarNumerosImpares() {
+		int cantNumerosImpares = 0;
+		for (int numero: numeros) {
+			if((numero % 2) != 0) {
+				cantNumerosImpares++;
+			}
+		}
+		return cantNumerosImpares;
+	}
+	
+	//Cuenta la cantidad de numeros multiplos del numero dado.
+	public int contarMultiplos(int numeroDado) {
+		int cantMultiplos = 0;
+		for(int numero:numeros) {
+			if((numero % numeroDado) == 0) {
+				cantMultiplos++;
+			}
+		}
+		return cantMultiplos;
+	}
+}
---
+++ test/ar/edu/unq/po2/tp3Test/CounterTestCase.java
@@ -0,0 +1,41 @@
+package ar.edu.unq.po2.tp3Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tp3.Counter;
+import junit.extensions.TestSetup;
+
+class CounterTestCase {
+
+	//Declaracion de variables
+	private Counter counter; //es de tipo Counter
+	
+	//setUp
+	@BeforeEach
+	public void setUp(){
+		//Asignacion de valor (INSTANCIAR VARIABLES)
+		counter = new Counter(); // instancio un nuevo Counter.
+		counter.agregarNumero(1);
+		counter.agregarNumero(3);
+		counter.agregarNumero(5);
+		counter.agregarNumero(7);
+		counter.agregarNumero(9);
+		counter.agregarNumero(1);
+		counter.agregarNumero(1);
+		counter.agregarNumero(1);
+		counter.agregarNumero(1);
+		counter.agregarNumero(4);
+	}
+	
+	
+	@Test
+	public void testPares() {
+		int cantidad = counter.contarNumerosImpares();
+		assertEquals(cantidad, 9);
+	}
+
+}
