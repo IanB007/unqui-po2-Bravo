1063c9268c1e48e067ba89a5ff7600a8a8f14785
IanB007 <Ian_b@192.168.0.70>
2024-04-04 19:08:49 -0300

    creacion de tests Rectangulo

--- src/ar/edu/unq/po2/tp3/Rectangulo.java
+++ src/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -1,5 +1,7 @@
 package ar.edu.unq.po2.tp3;
 
+import java.util.function.BooleanSupplier;
+
 public class Rectangulo {
 
 	//variables de instancia
@@ -17,7 +19,7 @@ public class Rectangulo {
 			 throw new IllegalArgumentException("La altura y el ancho no pueden ser iguales ya que es un rectangulo");
 		}
 		this.esquinaInicial = puntoInicial;
-		this.alto = altura;
+		this.alto = altura; // no hace falta el this porque tienen distinto nombre, en caso de que sean mismo nombre como en la linea 21, entonces ahi si deberia usar this.
 		this.ancho = ancho;
 	}
 
@@ -33,4 +35,21 @@ public class Rectangulo {
 		return ancho;
 	}
 
+	public int obtenerArea() {
+		return ancho * alto;
+	}
+
+	public int obtenerPerimetro() {
+		
+		return 2*(alto + ancho);
+	}
+
+	public boolean esHorizontal() {
+		boolean esHorizontal = true;
+		if (alto > ancho) {
+			esHorizontal = false;
+		}
+		return esHorizontal;
+	}
 }
+ 
--- test/ar/edu/unq/po2/tp3Test/RectanguloTestCase.java
+++ test/ar/edu/unq/po2/tp3Test/RectanguloTestCase.java
@@ -2,6 +2,7 @@ package ar.edu.unq.po2.tp3Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,30 +12,51 @@ import ar.edu.unq.po2.tp3.Rectangulo;
 
 public class RectanguloTestCase {
 
+	private Rectangulo rectangulo1;
+	private Rectangulo rectangulo2;
+	private Point esquinaSuperiorIzquierda1;
+	private Point esquinaSuperiorIzquierda2;
+	
 	
 	@BeforeEach
-	private void setUp() {
-		
+	private void setUp() {	
+		esquinaSuperiorIzquierda1 = new Point(4, 8);
+		esquinaSuperiorIzquierda2 = new Point(20, 5);
+		rectangulo1 = new Rectangulo(esquinaSuperiorIzquierda1, 2, 5); //horizontal
+		rectangulo2 = new Rectangulo(esquinaSuperiorIzquierda2, 8, 3); //vertical
 	}
 	
 	@Test
 	public void creacionDeRectangulo() {
-		Point esquinaSuperiorIzquierda = new Point(4, 5);
-		Rectangulo nuevoRectangulo = new Rectangulo(esquinaSuperiorIzquierda, 2, 5);
-		assertEquals(nuevoRectangulo.obtenerEsquinaInicial(), esquinaSuperiorIzquierda);
-		assertEquals(nuevoRectangulo.obtenerAlto(), 2);
-		assertEquals(nuevoRectangulo.obtenerAncho(), 5);
+		assertEquals(rectangulo1.obtenerEsquinaInicial(), esquinaSuperiorIzquierda1);
+		assertEquals(rectangulo1.obtenerAlto(), 2);
+		assertEquals(rectangulo1.obtenerAncho(), 5);
 	}
 	
 	@Test
 	public void creacionDeFalsoRectangulo() {
-		Point esquinaSuperiorIzquierda = new Point(4, 8);
 		
 		//Throwable o Exception? Throwable es la superclase de todas las excepciones pero es mejor exception?
 		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-			new Rectangulo(esquinaSuperiorIzquierda, 2, 2); //assertThrows
+			new Rectangulo(esquinaSuperiorIzquierda1, 2, 2); //assertThrows
 		});
 		
 		assertEquals(exception.getMessage(), "La altura y el ancho no pueden ser iguales ya que es un rectangulo");
+	}
+	
+	@Test
+	public void calcularArea() {
+		assertEquals(rectangulo1.obtenerArea(), 10);
+	}
+	
+	@Test
+	public void calcularPerimetro() {
+		assertEquals(rectangulo1.obtenerPerimetro(), 14);
+	}
+	
+	@Test
+	public void esRectanguloHorizontal() {
+		assertEquals(rectangulo1.esHorizontal(), true);
+		assertEquals(rectangulo2.esHorizontal(), false);
 	}
 }
