2900b79ae99d894fa0a7a92ce8e8c007b539b946
IanB007 <Ian_b@192.168.0.70>
2024-05-12 16:58:33 -0300

    creacion de tipo enum Color

---
+++ src/ar/edu/unq/po2/tp7/Color.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.po2.tp7;
+
+//para el tipo enum se coloca la palabra "enum" seguido del nombre del tipo enum.
+public enum Color {
+	ROJO("Prestar atencion esta en rojo!", "Riesgo muy alto", "gris"),
+	GRIS("atencion esta en griS!", "Riesgo alto", "amarillo"),
+	AMARILLO("Ey estas en amarillo!", "Riesgo medio", "miel"),
+	MIEL("Estas en miel!", "Riesgo bajo", "rojo");
+	
+	final String descripcion;
+	final String nivelDeRiesgo;
+	final String siguienteColor;
+	
+	Color(String descripcion, String nivelDeRiesgo, String siguienteColor){
+		this.descripcion = descripcion;
+		this.nivelDeRiesgo = nivelDeRiesgo;
+		this.siguienteColor = siguienteColor;
+	}
+}
